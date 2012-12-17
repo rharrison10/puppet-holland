8db5f7250eda1718f0a552ee3705371ea10ce63a
Russell Harrison <rharrison@fedoraproject.org>
2012-12-17 14:13:31 -0500

    Move augeas stuff to its own class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 # == Class: holland
 #
-# Common setup for the {Holland Backup Manager}[http://hollandbackup.org/].
+# Common setup for the {Holland Backup Manager}[http://hollandbackup.org/]. For most users these settings will not need to be
+# changed.
 #
 # === Parameters
 #
@@ -9,7 +10,8 @@
 # [*logfile*]
 #   The file Holland logs to
 # [*loglevel*]
-#   Sets the verbosity of Holland’s logging process. Available options are <tt>debug</tt>, <tt>info</tt>, <tt>warning</tt>, <tt>error</tt>, and <tt>critical</tt>
+#   Sets the verbosity of Holland’s logging process. Available options are <tt>debug</tt>, <tt>info</tt>, <tt>warning</tt>,
+#   <tt>error</tt>, and <tt>critical</tt>
 # [*path*]
 #   Defines a path for holland and its spawned processes
 # [*plugin_dirs*]
@@ -43,29 +45,13 @@
 #
 class holland (
   $backup_directory = '/var/spool/holland',
-  $logfile = '/var/log/holland/holland.log',
-  $loglevel = 'info',
-  $path = '/usr/local/bin:/usr/local/sbin:/bin:/sbin:/usr/bin:/usr/sbin',
-  $plugin_dirs = '/usr/share/holland/plugins',
-  $umask = '0007'
+  $logfile          = '/var/log/holland/holland.log',
+  $loglevel         = 'info',
+  $path             = '/usr/local/bin:/usr/local/sbin:/bin:/sbin:/usr/bin:/usr/sbin',
+  $plugin_dirs      = '/usr/share/holland/plugins',
+  $umask            = '0007'
 ) {
-  # There isn't an {Augeas}[http://augeas.net/] lens for +holland.conf+ in the upstream project yet so we'll need to provide one ourselves. Since Puppet requires +augeas-libs+ we don't need to manage the parrent directories since they'll already be in place.
-  file { '/usr/share/augeas/lenses/dist/holland.aug':
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    source => 'puppet:///modules/holland/augeas/holland.aug',
-  }
-
-  # Its also good practice to include the test file with the lens so we'll manage it as well.
-  file{'/usr/share/augeas/lenses/dist/tests/test_holland.aug':
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    source => 'puppet:///modules/holland/augeas/test_holland.aug',
-  }
+  include holland::augeas
 
   # The base Holland package
   package { 'holland':
@@ -74,22 +60,23 @@ class holland (
 
   validate_absolute_path($backup_directory)
   validate_absolute_path($logfile)
-  if !( $loglevel in ['debug', 'info', 'warning', 'error', 'critical'] ) {
+
+  if !($loglevel in ['debug', 'info', 'warning', 'error', 'critical']) {
     fail("loglevel = ${loglevel} must be one of debug, info, warning, error, or critical")
   }
-  $basic_changes = [
+  $basic_changes       = [
     "set holland/backup_directory ${backup_directory}",
     "set holland/path ${path}",
     "set holland/umask ${umask}",
     "set logging/filename ${logfile}",
-    "set logging/level ${loglevel}",
-  ]
+    "set logging/level ${loglevel}",]
 
-  #TODO test all plugin_dirs entries are valid file paths
+  # TODO test all plugin_dirs entries are valid file paths
   $plugin_dirs_changes = regsubst($plugin_dirs, '(.*)', 'set holland/plugin_dirs[ . = "\1" ] \1')
 
-  $conf_changes = flatten([ $basic_changes, $plugin_dirs_changes ])
+  $conf_changes        = flatten([$basic_changes, $plugin_dirs_changes])
 
+  # The file +holland.aug+ is managed by the +holland::augeas+ class
   augeas { '/etc/holland/holland.conf':
     context => '/files/etc/holland/holland.conf',
     changes => $conf_changes,
