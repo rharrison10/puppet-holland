636d8176fee935b7d2d839faa13173f0bec4d974
Russell Harrison <rharrison@fedoraproject.org>
2012-12-21 09:51:58 -0500

    Fix the sed regex

--- manifests/init.pp
+++ manifests/init.pp
@@ -93,7 +93,7 @@ class holland (
   # configuration file for this backup set which causes <tt>holland backup</tt> to fail.  This <tt>exec</tt> removes the backup set
   # if there isn't a backup set configuration file in place for it.  All backup set defines should notify this exec.
   exec { 'holland_remove_default_set':
-    command     => '/bin/sed -i -e \'s/\(^[[:space:]]*backupsets.*\),[[:space:]]*default[[:space:]]*$/\1/g\' -e \'s/\(^[[:space:]]*backupsets.*\)default[, ]*\(.\*\)/\1\2/g\' /etc/holland/holland.conf',
+    command     => '/bin/sed -i -e \'s/\(^[[:space:]]*backupsets.*\),[[:space:]]*default[[:space:]]*$/\1/g\' -e \'s/\(^[[:space:]]*backupsets.*\)default[, ]*\(.*\)/\1\2/g\' /etc/holland/holland.conf',
     onlyif      => '/bin/grep -q \'backupsets.*default\' /etc/holland/holland.conf',
     unless      => '/usr/bin/test -f /etc/holland/backupsets/default.conf',
     refreshonly => true,
