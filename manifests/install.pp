07fd4920308cc160c1bf1894a26ada3a9e3e002c
Joel Garboden <joel.garboden@logos.com>
2020-01-23 15:17:56 -0800

    Add Ubuntu package support

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,9 +7,31 @@
 class holland::install (
   Enum['absent', 'present'] $ensure = $::holland::ensure,
 ){
-  # The base Holland package
-  package { 'holland':
-    ensure => $ensure,
+  if $facts['os']['name'] in [ 'CentOS', 'RedHat', 'Scientific' ] {
+    # The base Holland package
+    package { 'holland':
+      ensure => $ensure,
+    }
+  } elsif $facts['os']['name'] == 'Ubuntu' {
+    require apt
+    apt::source { 'holland':
+      location => "https://download.opensuse.org/repositories/home:/holland-backup/x${facts['os']['name']}_${facts['os']['release']['major']}/",
+      release  => '',
+      repos    => './',
+      notify   => Exec['install_holland_package_key'],
+    }
+
+    exec { 'install_holland_package_key':
+      command     => "/usr/bin/wget -q -O - https://download.opensuse.org/repositories/home:/holland-backup/x${facts['os']['name']}_${facts['os']['release']['major']}/Release.key | apt-key add -",
+      refreshonly => true,
+      notify      => Class['apt::update'],
+    }
+    package { 'holland':
+      ensure  => $ensure,
+      require => Exec['install_holland_package_key'],
+    }
+  } else {
+    fail("${facts['os']['name']} unsupported")
   }
 
   $ensure_dir = $ensure ? {
